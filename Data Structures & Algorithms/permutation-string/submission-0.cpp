0231c7161c88734548e8ee998cbd4101bc9663d1
Henrique Mizevski <94652528+hen-mi@users.noreply.github.com>
2026-08-27 19:09:54 -0300

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int l = 0;
+        int k = s1.size();
+        vector<int> target(26, 0);
+
+        vector<int> currSum(26,0);
+        for(char& c : s1) {
+
+            target[c - 'a']++;
+        }
+
+        for(int r = 0; r != s2.size(); ++r) {
+
+            if(r - l >= k) {
+
+                currSum[s2[l] - 'a']--;
+                l++;
+            }
+         
+
+                currSum[s2[r] - 'a']++;
+
+                if(target == currSum) return true;
+        
+            
+
+        }
+
+
+        return false;
+    }
+};
